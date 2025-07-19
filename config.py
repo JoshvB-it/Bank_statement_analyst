4bb3f0d924d5cd7e4a85a93a79e0fdaddd8bc247
JoshvB-it <143925011+JoshvB-it@users.noreply.github.com>
2025-07-19 19:12:00 +0200

    Update config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# Password to access app
+PASSWORD = "Poen@enMilo131!"
+
+# Categories to classify transactions into
+CATEGORIES = {
+    "Rent": ["RENT"],
+    "Utilities": ["MUNICIPAL", "UTILITIES", "WATER", "ELECTRICITY"],
+    "WiFi": ["TELKOM", "VODACOM", "MTN", "RAIN", "AFRIBOOM", "INTERNET"],
+    "Food": ["SPAR", "WOOLWORTHS", "PICK N PAY", "CHECKERS", "FOOD"],
+    "Petrol": ["ENGEN", "SHELL", "BP", "SASOL", "TOTAL", "FUEL", "GARAGE"],
+    "Medical Aid": ["DISCOVERY", "MEDICAL", "HOSPITAL", "PHARMACY"],
+    "Income": ["SALARY", "INCOME", "TRANSFER FROM"],
+    "Airtime": ["AIRTIME", "RECHARGE"],
+    "Electricity": ["ELECTRICITY"],
+    "Insurance": ["INSURANCE", "OUTSURANCE", "CAR INSURANCE"],
+    "Other": []
+}
