493b3ddbc7f8c0b69d02db63a1c95ce1ef771b65
JoshvB-it <143925011+JoshvB-it@users.noreply.github.com>
2025-07-19 18:17:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
-APP_PASSWORD = "Poen@enMilo131!"
+PASSWORD = "Poen@enMilo131!"
+
+BUDGET = {
+    "Rent": 8000,
+    "Utilities": 1000,
+    "Wi-Fi": 600,
+    "Phones": 800,
+    "Petrol": 4000,
+    "Food": 6000,
+    "Miscellaneous": 1000,
+    "Insurance": 3000,
+    "Medical Aid": 7000,
+    "Income": 50000
+}
